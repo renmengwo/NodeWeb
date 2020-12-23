863227e6f6aaa85806dc5537cfe069292d4e056e
renmengwo <936777550@qq.com>
2020-12-23 10:28:01 +0800

    Node部署

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const http = require('http');
 const Router = require('./router/router')
-
+const port = 3000;
+const hostname = 'http://39.96.60.63'
 // 2.创建一个http服务对象监听用户请求的事件（req）
 http.createServer((req,res) =>{
     Router.statics(req, res, 'static');
-}).listen(8080,function(){
-    console.log('服务器启动，请访问：http://localhost:8080');
+}).listen(port,function(){
+    console.log(`服务器启动，请访问：${hostname}:${port}`);
 });
